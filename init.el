ba7d80e4e6ed7701832cb95b3eb76d1a8bfc15cd
Raghu Ugare <raghu.ugare@gmail.com>
2016-07-05 19:29:50 +0530

    init.el will be user-init-file & magit key-binding

---
+++ init.el
@@ -0,0 +1,25 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-archives
+   (quote
+    (("melpa-stable" . "https://stable.melpa.org/packages/")
+     ("melpa" . "https://melpa.org/packages/")
+     ("gnu" . "http://elpa.gnu.org/packages/")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+;; =========================================================
+;; --------------- KEY BINDINGS ----------------------------
+;; =========================================================
+
+;; Summoning magit-status popup via the popular 'C-x g' chord
+(global-set-key (kbd "C-x g") 'magit-status)
+
