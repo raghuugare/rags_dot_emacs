3376c0a6d045f6e5c5b4cfc7f95becf4e90a2903
Raghu Ugare <raghu.ugare@gmail.com>
2016-07-06 17:41:38 +0530

    Revamp init-file to use the excellent use-package macro!

--- init.el
+++ init.el
@@ -1,18 +1,14 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-archives
-   (quote
-    (("melpa-stable" . "https://stable.melpa.org/packages/")
-     ("melpa" . "https://melpa.org/packages/")
-     ("gnu" . "http://elpa.gnu.org/packages/"))))
- ;; Hide the menu-bar
- '(menu-bar-mode nil)
- ;; Hide the tool-bar
- '(tool-bar-mode nil)
- )
+(require 'package)
+
+;; prevent automatically loading all packages at start-up...
+(setq package-enable-at-startup nil)
+
+;; set-up package repos
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("elpa" . "http://elpa.gnu.org/packages/"))
+
+(package-initialize)
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -21,11 +17,32 @@
  ;; If there is more than one, they won't work right.
  )
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(menu-bar-mode nil)
+ '(tool-bar-mode nil))
 
-;; =========================================================
-;; --------------- KEY BINDINGS ----------------------------
-;; =========================================================
 
-;; Summoning magit-status popup via the popular 'C-x g' chord
-(global-set-key (kbd "C-x g") 'magit-status)
+;; First, ensure that we have the awesome use-package installed!
+;; Hat-tip to John Wiegley ! <3 :)
+;; Source: https://github.com/jwiegley/use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+(require 'diminish)
+(require 'bind-key)
+
+;; get magit for working with Git
+;; also bind its popular chord "C-x g" to magit-status pop-up!
+(use-package magit
+  :ensure t
+  :pin melpa-stable
+  :bind ("C-x g" . magit-status))
 
