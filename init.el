1bda033118099cfef072ba327ee24a7dd28c1fc3
Raghu Ugare <raghu.ugare@gmail.com>
2016-07-14 00:10:36 +0530

    Prefer the narrow version of Input Mono font.

--- init.el
+++ init.el
@@ -23,7 +23,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Input Mono")))))
+ '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Input Mono Narrow")))))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
