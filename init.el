8c975e6c05f5fda611a0a7cc4ffae31277387562
Raghu Ugare <raghu.ugare@gmail.com>
2016-08-01 12:46:23 +0530

    Show Column-number as well in the mode-line.

--- init.el
+++ init.el
@@ -30,6 +30,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
  '(indent-tab-mode nil t)
  '(menu-bar-mode nil)
  '(package-selected-packages
