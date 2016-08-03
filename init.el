9175400b4ca2664fbb40c5aed5e8a5844e051ce9
Raghu Ugare <raghu.ugare@gmail.com>
2016-08-03 20:37:04 +0530

    Remove background & foreground settings from face.

--- init.el
+++ init.el
@@ -23,7 +23,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#fdf6e3" :foreground "#657b83" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width extra-condensed :foundry "nil" :family "Input Mono Narrow")))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width extra-condensed :foundry "nil" :family "Input Mono Narrow")))))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
