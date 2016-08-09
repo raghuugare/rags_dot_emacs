5e735ffa7f7c32e3484ed993ac45e1602447c3de
Raghu Ugare <raghu.ugare@gmail.com>
2016-08-09 19:29:43 +0530

    Some aesthetic changes nay fixes!

--- init.el
+++ init.el
@@ -23,7 +23,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width extra-condensed :foundry "nil" :family "Input Mono Narrow")))))
+ '(default ((t (:inherit nil :stipple nil :background "#ede9e7" :foreground "#272b34" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Input Mono Narrow"))))
+ '(cursor ((t (:background "magenta4"))))
+ '(mode-line ((t (:background "wheat2" :foreground "#272b34" :box (:line-width 2 :color "grey75" :style released-button))))))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
