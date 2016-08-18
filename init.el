0a9aa3831091e0163fe4d7d46d2d85e2413b6006
Raghu Ugare <raghu.ugare@gmail.com>
2016-08-18 19:15:56 +0530

    CABAL help via company

--- init.el
+++ init.el
@@ -263,6 +263,10 @@ static char *gnus-pointer[] = {
 (use-package company-ghci
 	:ensure t)
 
+;; CABAL help via company
+(use-package company-cabal
+	:ensure t)
+
 ;; get the awesome smartparens package to manage pair-able stuff like (), [], HTML tags, etc.
 ;; Source: https://github.com/Fuco1/smartparens
 ;; Also see: https://ebzzry.github.io/emacs-pairs.html
